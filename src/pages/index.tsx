94c2853dc755c34772ddc13ed5bc1faae11e587b
Ciro Lemos <ciro.lemos01@gmail.com>
2024-06-12 13:31:29 -0300

    Primeiro acesso ao back node.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,35 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+
 export default function Home() {
+
+  const [message, setMessage] = useState('Loading')
+  const [participantes, setParticipantes] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3333/").then(
+      response => response.json()
+    ).then(
+      data => {
+        console.log(data)
+        setMessage(data.nome)
+        setParticipantes(data.participantes)
+      }
+    )
+  }, [])
+
   return (
-    <div className="flex justify-center items-center text-4xl h-screen">
-      Página Inicial
+    <div className="flex flex-col justify-center items-center text-4xl h-screen">
+      <span>{message}</span>
+      {
+        participantes.map((participantes, i) => (
+          <div className="text-base" key={i}>{participantes}</div>
+        ))
+      }
     </div>
   );
 }
+  
